dd948cd3616492d90931d3d4b0acbaf7a989713b
Lucas Rosa <lucasmetron@gmail.com>
2024-07-31 15:55:58 -0300

    teste

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
